b6a0b7d1918db1619e37a36f5a217bfbe74c6595
Hmtgit7 <hmtloharcoding3579@gmail.com>
2025-07-18 00:30:45 +0530

    feat(backend): add employee app models, migrations, and admin registration

---
+++ employee_management/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    department = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
